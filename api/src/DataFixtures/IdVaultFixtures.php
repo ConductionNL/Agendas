c196fb7949afd59591415361a97e871bacdbfb91
ginokok1996 <ginokok1996@hotmail.com>
2020-11-16 12:15:33 +0100

    fix

--- api/src/DataFixtures/IdVaultFixtures.php
+++ api/src/DataFixtures/IdVaultFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Calendar;
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class IdVaultFixtures extends Fixture
